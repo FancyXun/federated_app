2fb635be1ab475f9224e890e40accb271f1bb3eb
wheels97 <46299461+wheels97@users.noreply.github.com>
2020-09-30 13:09:09 +0800

    Update auc_get.py
    simplify the graph, just leaving the auc part

--- auc_get.py
+++ auc_get.py
@@ -1,70 +1,21 @@
 import tensorflow as tf
-import base_model
-import numpy as np
 
-class Regression(base_model.Model):
-    """
+graph = tf.Graph()
+nth = 1000
 
-    """
+def auc_tf(lab,pre,num_thresholds=1000):
+    auc_value, auc_op = tf.metrics.auc(lab, pre, num_thresholds=num_thresholds,name='auc_pair')
+    return auc_op
 
-    def __init__(self):
-        super().__init__()
-        self.learning_rate = 0.01
-        self.graph = tf.Graph()
-        self.placeholder_name = ['batch_size', 'x', 'y']
-        self.var = []
+with graph.as_default():
+    #auc_test
+    labels = tf.placeholder(shape=(None,), dtype=tf.float32,name='labels')
+    predictions = tf.placeholder(shape=(None,), dtype=tf.float32,name='predictions')
+    auc = auc_tf(labels,predictions,nth)
+    local_var = tf.local_variables()
+    init_new_vars_op = tf.initialize_variables(local_var)
 
-    def graph_generator(self):
-        """
-        :return:
-        """
-        with self.graph.as_default():
-            batch_size = tf.compat.v1.placeholder(tf.float32, name=self.placeholder_name[0])
-            x = tf.compat.v1.placeholder(tf.float32, [None, 141], name=self.placeholder_name[1])
-            y = tf.compat.v1.placeholder(tf.float32, [None, 2], name=self.placeholder_name[2])
-
-            #auc_test
-            labels = tf.placeholder(shape=(None,), dtype=tf.float32,name='labels')
-            predictions = tf.placeholder(shape=(None,), dtype=tf.float32,name='predictions')
-            auc = self.auc_tf(labels,predictions)
-            local_var = tf.local_variables()
-            init_new_vars_op = tf.initialize_variables(local_var)
-
-            w = tf.Variable(tf.zeros([141, 2]))
-            b = tf.Variable(tf.zeros([2]))
-            y_pre = tf.nn.softmax(tf.matmul(x, w) + b)
-            tf.reduce_mean(-tf.reduce_sum(y * tf.compat.v1.log(y_pre), axis=1), name="loss")
-            var = self.optimizer(x, y, y_pre, w, b, batch_size)
-            self.var = [w, b]
-            self.assign_var(var)
-
-    def optimizer(self, x, y, y_pre, w_init, b_init, batch_size):
-        """
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost, name="minimizeGradientDescent")
-        :return: update w and b
-        """
-        delta_y = y_pre - y
-        derivative_w = tf.matmul(tf.transpose(x), delta_y)
-        derivative_w = derivative_w / batch_size
-        derivative_b = tf.reduce_sum(delta_y, axis=0) / batch_size
-        w_new = w_init - self.learning_rate * derivative_w
-        b_new = b_init - self.learning_rate * derivative_b
-        var = [w_new, b_new]
-        return var
-
-    def optimizer_tf(self, cost):
-        # fixme: Bug still in android
-        return tf.train.GradientDescentOptimizer(self.learning_rate).minimize(cost, name="minimizeGradientDescent")
-
-    def auc_tf(self,lab,pre):
-        auc_value, auc_op = tf.metrics.auc(lab, pre, num_thresholds=1000,name='auc_pair')
-        return auc_op
-
-if __name__ == '__main__':
-    Regression().graph_generator()
-    print('project_ready!')
-
-
+tf.compat.v1.train.write_graph(graph,'./','getauc.pb',as_text=False)
 
 """ java code for calculating auc with a given labels_predictions pair
 
